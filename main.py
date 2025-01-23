a70919a1da4e386ae1d452c6d765106068c2659c
MrJWaring <james.waring@biswashington.org>
2025-01-23 14:54:57 +0000

    done my homework

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-a = 10
-b = 11
-print(a+b)
+with open("students.txt", "r") as file:
+    for line in file:
+        for char in line:
+            if char.isdigit():
+                print(char)
+                break
